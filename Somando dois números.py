c2abaa1fb5d0926f55ece7313678cfe9d7f27331
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-03 12:10:47 -0300

    Exercício 3

---
+++ Somando dois números.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+print(f"A soma entre {n1} e {n2} vale",n1+n2)
+
+# OU
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+soma = n1 + n2
+print(f"A soma entre {n1} e {n2} vale {soma}")
