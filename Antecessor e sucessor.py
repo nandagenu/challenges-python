e554cf072c2650bb5414bbf8fc50864a00f40687
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-05 09:13:21 -0300

    Exercício 5

---
+++ Antecessor e sucessor.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+valor = int(input("Digite um valor inteiro: "))
+suc = valor + 1
+ant = valor - 1
+print(f"O número sucessor de {valor} é {suc} e seu antecessor é {ant}.")
