c711c52a2085311e177e2808f4f46302de59c20d
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-05 09:12:29 -0300

    Exercício 4

---
+++ Dissecando uma variável.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+var = input("Digite uma palavra ou valor qualquer. ")
+
+print("Sobre essa variável, vamos analisar as seguintes informações:\n")
+print("Qual o tipo da variável?", type(var))
+print("Ela só tem espaços?", var.isspace())
+print("Ela é de ordem numérica?", var.isnumeric())
+print("É alfabético?", var.isalpha())
+print("É alfanumérico?", var.isalnum())
+print("Está em maiúsculo?", var.isupper())
+print("Está em minúsculo?", var.islower())
