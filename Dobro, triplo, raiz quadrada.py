8c2304fca32c90bdf802f4d0b7f7141351931d1e
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-05 09:17:32 -0300

    Exercício 6

--- Dobro, triplo, raiz quadrada.py
+++ Dobro, triplo, raiz quadrada.py
@@ -1,4 +1,4 @@
-# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e a raiz quadrada.
 
 number = float(input("Digite um número qualquer: "))
 print(f"O dobro de {number} é {number * 2}, seu triplo é {number * 3} e sua raiz quadrada {(number)**1/2}")
