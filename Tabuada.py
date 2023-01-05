5a8d1eb5da4a14e00ce188368884e03568ff6a60
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-05 09:35:35 -0300

    Exercício 9

---
+++ Tabuada.py
@@ -0,0 +1,14 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+number = int(input("Digite um número inteiro qualquer: "))
+print(f"\nA tabuada de {number} é:")
+print(f"{number}x1 = {number * 1}")
+print(f"{number}x2 = {number * 2}")
+print(f"{number}x3 = {number * 3}")
+print(f"{number}x4 = {number * 4}")
+print(f"{number}x5 = {number * 5}")
+print(f"{number}x6 = {number * 6}")
+print(f"{number}x7 = {number * 7}")
+print(f"{number}x8 = {number * 8}")
+print(f"{number}x9 = {number * 9}")
+print(f"{number}x10 = {number * 10}")
