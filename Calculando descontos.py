aba4630de3069a743fd6f07c72ebb6a90bbc59c0
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-06 11:04:45 -0300

    Exercício 12

---
+++ Calculando descontos.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+preço = float(input("Digite o preço do produto: "))
+print(f"Seu novo preço, com desconto de 5%, será de {preço*0.95} reais.")
