08fd1f5a84faee4f56390585b464dc4aab89cff4
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-06 11:05:27 -0300

    Exercício 13

---
+++ Reajuste salarial.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input("Digite o valor do seu salário: "))
+print(f"O seu salário, com aumento de 15%, ficará em {salario*1.15} reais.")
