ef372d2419be3a742b47389c01dbf5c803de1f08
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-10 12:02:22 -0300

    Exercício 46

---
+++ Contagem regressiva.py
@@ -0,0 +1,9 @@
+'''Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
+indo de 10 até 0, com uma pausa de 1 segundo entre eles.'''
+
+from time import sleep
+print("Contagem regressiva...")
+for count in range(10, 0, -1):
+    print(count)
+    sleep(1)
+print("FIM!")
