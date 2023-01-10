c2f20cb9a0276c207bb92decd481244ccc45eb3c
Fernanda Genú <nandafmgenu@gmail.com>
2023-01-10 13:14:28 -0300

    Exercício 47

---
+++ Contagem de pares.py
@@ -0,0 +1,10 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+'''for npar in range(0, 52, 2):
+    print(npar)'''
+
+# OU
+
+for npar in range(0, 52):
+    if npar % 2 == 0:
+        print(npar)
