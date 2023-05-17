16cc3ba3aad9bc5a0ac53c8cfb1e619a6b751d9d
Fernanda Genú <nandafmgenu@gmail.com>
2023-05-17 13:01:08 -0300

    Exercício 49

---
+++ Tabuada 2.0.py
@@ -0,0 +1,6 @@
+# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+print("Tabuada do nº 4:")
+for i in range(1,10):
+  print(f"4x{i} = {4*i}")
+  
